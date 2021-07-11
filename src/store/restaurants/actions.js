6abd866dbd8ee1379e5770e1b3ac61f163c8e973
fabious <fabien@les-tilleuls.coop>
2021-07-11 11:51:21 +0200

    restaurants reducer test

--- src/store/restaurants/actions.js
+++ src/store/restaurants/actions.js
@@ -1,1 +1,12 @@
-export const loadRestaurants = () => () => {};
+export const STORE_RESTAURANT = 'STORE_RESTAURANT';
+
+const storeRestaurants = records => ({
+  type: STORE_RESTAURANT,
+  records,
+});
+
+export const loadRestaurants = () => (dispatch, getState, api) => {
+  api.loadRestaurants().then(records => {
+    dispatch(storeRestaurants(records));
+  });
+};
--- src/store/restaurants/reducers.js
+++ src/store/restaurants/reducers.js
@@ -1,5 +1,13 @@
 import {combineReducers} from 'redux';
+import {STORE_RESTAURANT} from './actions';
 
-const records = () => [];
+const records = (state = [], action) => {
+  switch (action.type) {
+    case STORE_RESTAURANT:
+      return action.records;
+    default:
+      return state;
+  }
+};
 
 export default combineReducers({records});
