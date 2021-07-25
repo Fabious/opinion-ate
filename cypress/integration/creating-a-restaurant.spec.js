b2e3f8ca923f6d7ec6f84b1a81c850c4a0962c5a
fabious <fabien@les-tilleuls.coop>
2021-07-25 17:34:56 +0200

    Feat: add restaurant

---
+++ cypress/integration/creating-a-restaurant.spec.js
@@ -0,0 +1,36 @@
+describe('Create a Restaurant', () => {
+  it('allows adding restaurants', () => {
+    const restaurantId = 27;
+    const restaurantName = 'Sushi Place';
+
+    cy.intercept(
+      'GET',
+      `https://outside-in-dev-api.herokuapp.com/${Cypress.env(
+        'apiKey',
+      )}/restaurants`,
+      [],
+    );
+
+    cy.intercept(
+      'POST',
+      `https://outside-in-dev-api.herokuapp.com/${Cypress.env(
+        'apiKey',
+      )}/restaurants`,
+      {
+        id: restaurantId,
+        name: restaurantName,
+      },
+    ).as('addRestaurant');
+
+    cy.visit('/');
+
+    cy.get('[placeholder="Add Restaurant"]').type(restaurantName);
+    cy.contains('Add').click();
+
+    cy.wait('@addRestaurant').its('request.body').should('deep.equal', {
+      name: restaurantName,
+    });
+
+    cy.contains(restaurantName);
+  });
+});
--- src/store/restaurants/reducers.js
+++ src/store/restaurants/reducers.js
@@ -1,10 +1,17 @@
 import {combineReducers} from 'redux';
-import {START_LOADING, STORE_RESTAURANT, RECORD_LOADING_ERROR} from './actions';
+import {
+  START_LOADING,
+  STORE_RESTAURANT,
+  RECORD_LOADING_ERROR,
+  ADD_RESTAURANT,
+} from './actions';
 
 const records = (state = [], action) => {
   switch (action.type) {
     case STORE_RESTAURANT:
       return action.records;
+    case ADD_RESTAURANT:
+      return [...state, action.record];
     default:
       return state;
   }
